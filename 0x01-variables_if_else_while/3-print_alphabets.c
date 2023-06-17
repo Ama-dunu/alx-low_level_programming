45974dbd89f7b76b9b9a04e3e9097f76f43e2da0
Ama-dunu <annettebdunu@gmail.com>
2023-06-16 17:07:13 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,14 +5,10 @@ int main(void)
 char lowercase, uppercase;
 
 for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-{
-	    putchar(lowercase);
-}
+	putchar(lowercase);
 
 for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-{
-	    putchar(uppercase);
-}
+	putchar(uppercase);
 
 putchar('\n');
 
