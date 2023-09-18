ac2a8bf012764866f091c809072dfc60a97fea20
Ama-dunu <annettebdunu90@gmail.com>
2023-09-18 13:09:33 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/2-strncpy.c
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -1,16 +1,17 @@
 #include "main.h"
+
 /**
  * _strncpy - copy a string
  * @dest: input value
  * @src: input value
  * @n: input value
- *
  * Return: dest
  */
+
 char *_strncpy(char *dest, char *src, int n)
+
 {
 	int j;
-
 
 	j = 0;
 	while (j < n && src[j] != '\0')
@@ -23,7 +24,6 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[j] = '\0';
 		j++;
 	}
-
-
 	return (dest);
 }
+
--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
-
 /**
- * _abs - computes the absolute value of an integer
+ * _abs - Computes the absolute value of an integer.
  * @n: the int to check
- * Return: the absolute value of int
+ * Return: Absolute value of int
  */
 int _abs(int n)
 {
