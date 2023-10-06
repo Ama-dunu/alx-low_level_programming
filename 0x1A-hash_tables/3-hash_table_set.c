61d35f361dd930b966f194096b554808af5b7efa
Ama-dunu <annettebdunu90@gmail.com>
2023-10-06 11:10:10 -0700

    0x1A. C - Hash tables

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,26 @@
+ray[i]->key, key) == 0)
+		{
+			free(ht->array[i]->value);
+			ht->array[i]->value = value_copy;
+			return (1);
+		}
+	}
+
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		free(value_copy);
+		return (0);
+	}
+	new->key = strdup(key);
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = value_copy;
+	new->next = ht->array[index];
+	ht->array[index] = new;
+
+	return (1);
+}
